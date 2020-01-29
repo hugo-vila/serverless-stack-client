47c909d7831e03b0fa13bd9a5e96c31861193b5a
Hugo Vila <hvila3@gmail.com>
2020-01-29 19:03:40 +0100

    feat: Give feedback while logging in

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -14,6 +16,8 @@ export default function Login(props) {
   async function handleSubmit(event) {
     event.preventDefault();
 
+    setIsLoading(true);
+
     try {
       await Auth.signIn(email, password);
       props.userHasAuthenticated(true);
@@ -21,6 +25,7 @@ export default function Login(props) {
       alert("Logged In!");
     } catch (e) {
       alert(e.message);
+      setIsLoading(false);
     }
   }
 
@@ -44,9 +49,15 @@ export default function Login(props) {
             type="password"
           />
         </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <LoaderButton
+          block
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+          type="submit"
+        >
           Login
-        </Button>
+        </LoaderButton>
       </form>
     </div>
   );
