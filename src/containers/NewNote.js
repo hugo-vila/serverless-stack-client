3dfa9aafed1d387897159694d9c380003a2b8d49
Hugo Vila <hvila3@gmail.com>
2020-02-04 19:47:23 +0100

    feat: Upload before Creating a Note

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 import config from "../config";
 import "./NewNote.css";
 
@@ -32,11 +33,12 @@ export default function NewNote(props) {
     setIsLoading(true);
 
     try {
-      await createNote({ content });
+      const attachment = file.current ? await s3Upload(file.current) : null;
+
+      await createNote({ content, attachment });
       props.history.push("/");
     } catch (e) {
       alert(e);
-      console.log(e);
       setIsLoading(false);
     }
   }
