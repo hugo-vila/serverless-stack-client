e8efcd95e522f4dfdbad7dcc7e0129fd99db7320
Hugo Vila <hvila3@gmail.com>
2020-02-11 14:14:39 +0100

    Use the Redirect Routes

--- src/Routes.js
+++ src/Routes.js
@@ -8,21 +8,43 @@ import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-      <AppliedRoute
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
         path="/signup"
         exact
         component={Signup}
         appProps={appProps}
       />
-      <AppliedRoute path="/notes/new" exact component={NewNote} appProps />
-      <AppliedRoute path="/notes/:id" exact component={Notes} appProps />
-      <AppliedRoute path="/settings" exact component={Settings} appProps />
+      <AuthenticatedRoute
+        path="/notes/new"
+        exact
+        component={NewNote}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/notes/:id"
+        exact
+        component={Notes}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/settings"
+        exact
+        component={Settings}
+        appProps={appProps}
+      />
       <Route component={NotFound} />
     </Switch>
   );
