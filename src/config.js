360abf2f25556643d7cbe0dc323230ae53e8b2ca
Hugo Vila <hvila3@gmail.com>
2020-02-24 12:50:03 +0100

    chore: Integrate the Amplify configuration into the general configuration file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
+import config from "./amplify/config";
+
+const { s3, apiGateway, cognito } = config;
+
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
-  STRIPE_KEY: "pk_test_0uUHYyI9hiKCVRS6x2nzdL1i"
+  STRIPE_KEY: "pk_test_0uUHYyI9hiKCVRS6x2nzdL1i",
+  s3,
+  apiGateway,
+  cognito
 };
--- src/index.js
+++ src/index.js
@@ -5,27 +5,29 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import Amplify from "aws-amplify";
-import config from "./amplify/config";
+import config from "./config";
+
+const { s3, apiGateway, cognito } = config;
 
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    region: cognito.REGION,
+    userPoolId: cognito.USER_POOL_ID,
+    identityPoolId: cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: cognito.APP_CLIENT_ID
   },
   Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID
+    region: s3.REGION,
+    bucket: s3.BUCKET,
+    identityPoolId: cognito.IDENTITY_POOL_ID
   },
   API: {
     endpoints: [
       {
         name: "notes",
-        endpoint: config.apiGateway.URL,
-        region: config.apiGateway.REGION
+        endpoint: apiGateway.URL,
+        region: apiGateway.REGION
       }
     ]
   }
