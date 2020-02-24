86b629f7f7cb2d61c53c1d7a3a6339cb716532d1
Hugo Vila <hvila3@gmail.com>
2020-02-24 14:10:38 +0100

    chore: Create development and production stage for App configuration

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 import config from "./amplify/config";
 
-const { s3, apiGateway, cognito } = config;
+const { dev, prod } = config;
+
+// Default to dev if not set
+const stage = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
 
 export default {
+  // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
   STRIPE_KEY: "pk_test_0uUHYyI9hiKCVRS6x2nzdL1i",
-  s3,
-  apiGateway,
-  cognito
+  ...stage
 };
