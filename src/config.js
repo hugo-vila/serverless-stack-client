f48c2a4418ef4ddbc88a3a54c5e55454aa850e0d
Hugo Vila <hvila3@gmail.com>
2020-02-25 14:17:11 +0100

    chore: extract hard coded Stripe Key from general configuration file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
 import config from "./amplify/config";
+import stripe from "./stripe/";
 
 const { dev, prod } = config;
+
+let STRIPE_KEY = {};
+
+if (process.env.REACT_APP_STAGE === "prod") {
+  STRIPE_KEY = stripe[process.env.REACT_APP_STAGE].STRIPE_KEY;
+} else {
+  STRIPE_KEY = stripe.dev.STRIPE_KEY;
+}
 
 // Default to dev if not set
 const stage = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
@@ -8,6 +17,6 @@ const stage = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-  STRIPE_KEY: "pk_test_0uUHYyI9hiKCVRS6x2nzdL1i",
+  STRIPE_KEY,
   ...stage
 };
