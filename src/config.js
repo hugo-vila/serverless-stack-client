577b70ce043153c0a77719f7c061c0c4a38d54ed
Hugo Vila <hvila3@gmail.com>
2020-03-05 17:19:00 +0100

    chore: configure config.js to use .env.development file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,42 @@
-import config from "./amplify/config";
-import stripe from "./stripe/";
-
+const config = {
+  dev: {
+    cognito: {
+      REGION: process.env.REACT_APP_DEV_COGNITO_REGION,
+      USER_POOL_ID: process.env.REACT_APP_DEV_COGNITO_USER_POOL_ID,
+      IDENTITY_POOL_ID: process.env.REACT_APP_DEV_COGNITO_IDENTITY_POOL_ID,
+      APP_CLIENT_ID: process.env.REACT_APP_DEV_COGNITO_APP_CLIENT_ID
+    },
+    s3: {
+      REGION: process.env.REACT_APP_DEV_S3_REGION,
+      BUCKET: process.env.REACT_APP_DEV_S3_BUCKET
+    },
+    apiGateway: {
+      REGION: process.env.REACT_APP_DEV_APIGATEWAY_REGION,
+      URL: process.env.REACT_APP_DEV_APIGATEWAY_URL
+    }
+  },
+  prod: {
+    cognito: {
+      REGION: process.env.REACT_APP_PROD_COGNITO_REGION,
+      USER_POOL_ID: process.env.REACT_APP_PROD_COGNITO_USER_POOL_ID,
+      IDENTITY_POOL_ID: process.env.REACT_APP_PROD_COGNITO_IDENTITY_POOL_ID,
+      APP_CLIENT_ID: process.env.REACT_APP_PROD_COGNITO_APP_CLIENT_ID
+    },
+    s3: {
+      REGION: process.env.REACT_APP_PROD_S3_REGION,
+      BUCKET: process.env.REACT_APP_PROD_S3_BUCKET
+    },
+    apiGateway: {
+      REGION: process.env.REACT_APP_PROD_APIGATEWAY_REGION,
+      URL: process.env.REACT_APP_PROD_APIGATEWAY_URL
+    }
+  }
+};
 const { dev, prod } = config;
+const stripe = {
+  dev: { STRIPE_KEY: process.env.REACT_APP_DEV_STRIPE_KEY },
+  prod: { STRIPE_KEY: process.env.REACT_APP_PROD_STRIPE_KEY }
+};
 
 let STRIPE_KEY = {};
 
@@ -11,7 +46,6 @@ if (process.env.REACT_APP_STAGE === "prod") {
   STRIPE_KEY = stripe.dev.STRIPE_KEY;
 }
 
-// Default to dev if not set
 const stage = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
 
 export default {
